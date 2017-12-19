9e3590ffd412a6e09d9fba7ea6382b6a1e4a2ee9
gurigura <penguin@Penguin-no-MacBook-Air.local>
2017-12-19 13:37:01 +0900

    メニューをコンポーネント化

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,7 @@
     <div id="top" class="header">
       <h1 class="title">vue.js nav</h1>
 
-      <ul class="nav nav-pills">
-        <li class="nav-item">
-          <router-link v-bind:class="{ active: $route.path === '/' }" class="nav-link" to="/">Home</router-link>
-        </li>
-        <li class="nav-item dropdown">
-          <router-link v-bind:class="{ active: $route.path === '/page1' || $route.path === '/page1/2' }" class="nav-link dropdown-toggle" data-toggle="dropdown" to="#" role="button" aria-haspopup="true" aria-expanded="false">Page1</router-link>
-          <div class="dropdown-menu">
-            <router-link v-bind:class="{ active: $route.path === '/page1' }" class="dropdown-item" to="/page1">Page1</router-link>
-            <router-link v-bind:class="{ active: $route.path === '/page1/2' }" class="dropdown-item" to="/page1/2">Page1-2</router-link>
-          </div>
-        </li>
-        <li class="nav-item">
-          <router-link v-bind:class="{ active: $route.path === '/page2' }" class="nav-link" to="/page2">Page2</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link v-bind:class="{ active: $route.path === '/page3' }" class="nav-link" to="/page3">Page3</router-link>
-        </li>
-      </ul>
+      <menu_nav></menu_nav>
     </div>
 
     <div class="content">
@@ -28,6 +11,15 @@
     </div>
   </div>
 </template>
+
+<script>
+  import Vue from 'vue'
+  import nav from './components/lib/nav'
+
+  Vue.component('menu_nav', nav)
+
+  export default{}
+</script>
 
 <style>
   html {
---
+++ src/components/lib/nav.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul class="nav nav-pills">
+    <li class="nav-item">
+      <router-link v-bind:class="{ active: $route.path === '/' }" class="nav-link" to="/">Home</router-link>
+    </li>
+    <li class="nav-item dropdown">
+      <router-link v-bind:class="{ active: $route.path === '/page1' || $route.path === '/page1/2' }" class="nav-link dropdown-toggle" data-toggle="dropdown" to="#" role="button" aria-haspopup="true" aria-expanded="false">Page1</router-link>
+      <div class="dropdown-menu">
+        <router-link v-bind:class="{ active: $route.path === '/page1' }" class="dropdown-item" to="/page1">Page1</router-link>
+        <router-link v-bind:class="{ active: $route.path === '/page1/2' }" class="dropdown-item" to="/page1/2">Page1-2</router-link>
+      </div>
+    </li>
+    <li class="nav-item">
+      <router-link v-bind:class="{ active: $route.path === '/page2' }" class="nav-link" to="/page2">Page2</router-link>
+    </li>
+  </ul>
+</template>
